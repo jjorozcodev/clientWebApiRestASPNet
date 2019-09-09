fe44b091695d8094a515da9a5969db6174f3cd2a
JJ <jj.orozcolopez.13@gmail.com>
2019-09-09 00:18:57 -0600

    Adding the MVC Controller
    Using MVC 5 Controller empty, and added method for receive data from BaseURL/api/products

---
+++ myClientWebApi/Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using myClientWebApi.Models;
+using Newtonsoft.Json;
+
+namespace myClientWebApi.Controllers
+{
+    public class ProductsController : Controller
+    {
+
+        private string BaseURL = "http://localhost:54041/";
+
+        public async Task<ActionResult> Index()
+        {
+            List<Product> listProducts = new List<Product>();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseURL);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage resp = await client.GetAsync("api/products/");
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    var prodResp = resp.Content.ReadAsStringAsync().Result;
+                    listProducts = JsonConvert.DeserializeObject<List<Product>>(prodResp);
+                }
+
+                return View(listProducts);
+            }
+        }
+    }
+}
