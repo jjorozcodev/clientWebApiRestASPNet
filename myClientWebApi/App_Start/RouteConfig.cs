ce936f27d39bd8cdede8566856eb9adad9f69332
JJ <jj.orozcolopez.13@gmail.com>
2019-09-13 07:19:20 -0600

    View and methods for Create new Product
    Created View for new Product and method to send the object to the API Rest.

--- myClientWebApi/App_Start/RouteConfig.cs
+++ myClientWebApi/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace myClientWebApi
@@ -16,7 +12,7 @@ namespace myClientWebApi
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Products", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
--- myClientWebApi/Controllers/ProductsController.cs
+++ myClientWebApi/Controllers/ProductsController.cs
@@ -34,5 +34,33 @@ namespace myClientWebApi.Controllers
                 return View(listProducts);
             }
         }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            //Complete all fields...
+            product.CategoryID = 1;
+            product.SupplierID = 1;
+            product.Discontinued = false;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseURL + "/api/products/");
+                var postTask = client.PostAsJsonAsync<Product>("products", product);
+                postTask.Wait();
+                var result = postTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            ModelState.AddModelError(string.Empty, "Error: Please, contact the administrator.");
+            return View(product);
+        }
     }
 }
